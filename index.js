00eaef066c255cbeb9378648273bb39d5ca0079d
Mark Kauffman <markkauffman22@gmail.com>
2019-09-17 02:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Mark Kauffman
 
 function add(a, b) {
-  return (a + b)
+  return (a + b);
+}
+
+function subtract(a, b) {
+  return (a - b);
+}
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+function divide(a, b) {
+  return (a / b);
 }
 
 
